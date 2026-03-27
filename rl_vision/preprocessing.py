11149237510f80f0074fba6ecd6fa2ca1e692748
ZyroEolu-sk <shengkai.zhu@alumnos.upm.es>
2026-03-27 22:11:27 +0100

    add: personalized sizes for image

--- rl_vision/preprocessing.py
+++ rl_vision/preprocessing.py
@@ -1,11 +1,10 @@
 import cv2
 
-
-def preprocessing(image):
+def preprocessing(image, new_height, new_width):
     # Convert to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Resize to 84x84
-    resized = cv2.resize(gray, (84, 84))
+    resized = cv2.resize(gray, (new_height, new_height))
     # Normalize pixel values to [0, 1]
     normalized = resized / 255.0
     return normalized
